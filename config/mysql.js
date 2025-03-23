eeb0de579cd249fe1a535a13c3290d3bcf780eec
Niño Anthony Bandalan <anonimity.love20@gmail.com>
2025-03-24 03:03:41 +0800

    Fix mysql connection

--- config/mysql.js
+++ config/mysql.js
@@ -9,7 +9,7 @@ require('dotenv').config();
 //   database: process.env.DB_NAME,
 // });
 
-const db = process.env.DATABASE_URL;
+const db = `mysql://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
 
 const pool = mysql.createPool(db);
 
